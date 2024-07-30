b1f81a76f784bbc9f9e1576e31840bc2b7a7f691
e4e6 <114649461+e4e6@users.noreply.github.com>
2024-07-30 16:24:58 +0900

    프로바이더 및 테스트용 editpage화면 추가

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:memove_practice/data/models/text_model.dart';
 
 class TaskModel extends ChangeNotifier {
-  TextModel title;
+  String title;
+  String detail;
   bool isChecked;
 
   TaskModel({
     required this.title,
+    this.detail='No detail in TaskModel.dart',
     this.isChecked = false,
   });
 }
--- lib/modules/home/controllers/home_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:memove_practice/data/models/task_list_model.dart';
-import 'package:memove_practice/data/models/task_model.dart';
-
-class HomePageCtrl {
-  static void addMemo(TaskModel memo)
-  {
-    TaskListModel.addMemo(memo);
-  }
-}
