f25be4ab08e8866fc76ff66f6e4cc1fc1c0b79cd
wys47 <ys.mr892@gmail.com>
2024-08-05 21:39:27 +0900

    dd

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -67,61 +67,63 @@ class HomePageState extends State<HomePage> {
                     physics: NeverScrollableScrollPhysics(),
                     itemCount: taskData.taskCount,
                     itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                              pageBuilder: (context, animation, secondaryAnimation) =>
-                                  TaskEditPage(taskIndex: index),
-                              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                return FadeTransition(
-                                  opacity: animation,
-                                  child: child,
-                                );
-                              },
-                              transitionDuration: Duration(milliseconds: 150), // 전환 지속 시간 설정
-                            ),
-                          );
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              height: 80,
-                              margin: EdgeInsets.only(bottom: 10),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: getLighterColor(mainColor, 0.85),
+                      if (!taskData.taskList[index].isChecked) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder: (context, animation, secondaryAnimation) =>
+                                    TaskEditPage(taskIndex: index),
+                                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                  return FadeTransition(
+                                    opacity: animation,
+                                    child: child,
+                                  );
+                                },
+                                transitionDuration: Duration(milliseconds: 150), // 전환 지속 시간 설정
                               ),
-                              alignment: Alignment.center,
-                              child: Text(
-                                taskData.taskList[index].title.content,
-                                style: TextStyle(
-                                  fontSize: taskData.taskList[index].title.size,
-                                  color:  taskData.taskList[index].title.isChangeColor ? _textColors[_currentColorIndex] : Colors.black, // 텍스트 색상 설정
-                                  decoration: taskData.taskList[index].isChecked
-                                      ? TextDecoration.lineThrough
-                                      : null,
+                            );
+                          },
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: 80,
+                                margin: EdgeInsets.only(bottom: 10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: getLighterColor(mainColor, 0.85),
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(
+                                  taskData.taskList[index].title.content,
+                                  style: TextStyle(
+                                    fontSize: taskData.taskList[index].title.size,
+                                    color:  taskData.taskList[index].title.isChangeColor ? _textColors[_currentColorIndex] : Colors.black, // 텍스트 색상 설정
+                                    decoration: taskData.taskList[index].isChecked
+                                        ? TextDecoration.lineThrough
+                                        : null,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Align(
-                                alignment: Alignment.centerLeft,
-                                child: Container(
-                                  height: 80,
-                                  width: 80,
-                                  child: IconButton(
-                                      onPressed: () {
-                                        taskData.changeCheckState(index: index);
-                                        taskData.changeIsStrikeThrough(index: index);
-                                      },
-                                      icon: Icon(taskData.taskList[index].isChecked
-                                          ? Icons.check
-                                          : Icons.square_outlined)),
-                                ))
-                          ],
-                        ),
-                      );
+                              Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Container(
+                                    height: 80,
+                                    width: 80,
+                                    child: IconButton(
+                                        onPressed: () {
+                                          taskData.changeCheckState(index: index);
+                                          taskData.changeIsStrikeThrough(index: index);
+                                        },
+                                        icon: Icon(taskData.taskList[index].isChecked
+                                            ? Icons.check
+                                            : Icons.square_outlined)),
+                                  ))
+                            ],
+                          ),
+                        );
+                      }
                     },
                   ),
                 ],
